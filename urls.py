93d6cc73cb1cd2edbad43ed7aecd21f12ec695cf
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-08-30 17:04:10 -0400

    Initial commit of geo views and their underlying infrastructure.

--- urls.py
+++ urls.py
@@ -7,18 +7,6 @@ from django.conf import settings
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', direct_to_template, {'template': 'index.html'}, name='index'),
-    # url(r'^muni_words/', include('muni_words.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
-
-
-urlpatterns += patterns('',
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'', include('scrape_cc.urls')),
 )
